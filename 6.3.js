b7c5baf5070f673f8fb9936828fdc3fcc43c500f
23kb1a3092-ctrl <23kb1a3092@nbkrist.org>
2025-09-15 14:03:34 +0530

    Add files via upload

---
+++ 6.3.js
@@ -0,0 +1,20 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head><title>Hello</title></head>
+      <body>
+        <h1>Check your browser console!</h1>
+        <script>console.log('Hello World');</script>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
